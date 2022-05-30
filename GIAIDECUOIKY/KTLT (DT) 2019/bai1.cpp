2ff7c87f2e5177df1f16c499052642f15511e61f
wiliam29 <95753623+cogi2996@users.noreply.github.com>
2022-05-30 17:07:48 +0700

    Update bai1.cpp

--- GIAIDECUOIKY/KTLT (DT) 2019/bai1.cpp
+++ GIAIDECUOIKY/KTLT (DT) 2019/bai1.cpp
@@ -1,27 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-	int a = 2,b  =4,c = 6;
-	int m;
-	cin>>m;
-	int f = 0;
-	int temp = 2;
-	while(f <= m)
-	{
-		f = f + (a*2+b*4+c*6);
-		a = b;
-		b = c;
-		c = f;
-		temp++;
-	}
-	if(m==2)
-		cout<<0;
-	else if(m==4)
-		cout<<1;
-	else if(m == 6)
-		cout<<2;
-	else
-		cout<<--temp;
+int xet(int n){
+    if(n==0)
+        return 2;
+    else if(n == 1)
+        return 4;
+    else if(n== 2)
+        return 6;
+    return xet(n-3)*2 + xet(n-2)*4 + xet(n-1)*6;
+        
+    }   
+int main()
+{
+    int m; cin>>m;
+    int n;
+    while(xet(n)<=m)
+        n++;
+    cout<<n-1;
 }
-
